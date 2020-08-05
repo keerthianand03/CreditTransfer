a6f4a8f487a9235211e83071c212b58910e5b369
keerthianand03 <60270758+keerthianand03@users.noreply.github.com>
2020-08-05 23:49:59 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,36 @@
+<!doctype html>
+<html>
+<head>
+<style>
+body  {
+  background-image: url("images.jpg");
+  background-color: #cccccc;
+  background-repeat: no-repeat;
+  background-size: 1600px 1500px;
+ background-blend-mode: overlay;
+}
+</style>
+<style>
+.banner{
+width:800px;
+height:200px;
+margin:7px auto;
+-webkit-border-shadow:0 1px 3px rgba(0,0,0,0.5);
+-webkit-border-radius:25px;
+padding-left:50px;
+font-family:"Times New Roman";
+letter-spacing:5px;
+font-size:50px;
+}
+</style>
+</head>
+<body style="font-family:Comic Sans MS;font-size:160%;color:blue;">
+
+<div class="banner"><center></br>Welcome to Credit Management</center></div>
+<br><br>
+<h3> 
+<center><a href="ViewUsers.php"> View all Users>></center>
+</h3>
+
+</body>
+</html>
